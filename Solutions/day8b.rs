20ad3c9d6476aedeb27b9c8eb43e12835cea625b
goosbo <keb1898@protonmail.com>
2024-12-08 11:50:41 +0530

    day 8

---
+++ Solutions/day8b.rs
@@ -0,0 +1,75 @@
+use std::io;
+use std::collections::HashMap;
+
+fn main(){
+    let mut antennas:HashMap<char, Vec<(usize,usize)>>= HashMap::new();
+    let mut grid:Vec<Vec<char>> = vec![];
+    loop{
+        let mut line = String::new();
+        io::stdin().read_line(&mut line).unwrap();
+
+        if line.trim() == ""{
+            break;
+        }
+
+        grid.push(line.trim().chars().collect());
+    }
+
+
+    for i in 0..grid.len(){
+        for j in 0..grid[i].len(){
+            if grid[i][j] != '.'{
+                match antennas.get_mut(&grid[i][j]) {
+                    Some(freq) =>{
+                        freq.push((i,j));
+                    },
+                    None =>{
+                        antennas.insert(grid[i][j], vec![(i,j)]);
+                    }
+                }
+            }
+        }
+    }
+
+    let mut antinodes = grid.clone();
+
+    for (_freq, fantennas) in antennas.iter(){
+        for i in 0..fantennas.len(){
+            antinodes[fantennas[i].0][fantennas[i].1] = '#';
+            for j in i+1..fantennas.len(){
+
+                let coord0:(isize,isize) = (fantennas[i].0 as isize,fantennas[i].1 as isize);
+                let coord1:(isize,isize) = (fantennas[j].0 as isize,fantennas[j].1 as isize);
+
+                let delta_i:isize = coord1.0 - coord0.0;
+                let delta_j:isize = coord1.1 - coord0.1;
+
+                let mut antinode0 = (coord0.0-delta_i,coord0.1-delta_j);
+                while (antinode0.0 >= 0 && antinode0.0 < grid.len() as isize) && (antinode0.1 >= 0 && antinode0.1 < grid[0].len() as isize){
+                    antinodes[antinode0.0 as usize][antinode0.1 as usize] = '#';
+                    antinode0.0 -= delta_i;
+                    antinode0.1 -= delta_j;
+                }
+
+                let mut antinode1 = (coord1.0+delta_i,coord1.1+delta_j);
+                while (antinode1.0 >= 0 && antinode1.0 < grid.len() as isize) && (antinode1.1 >= 0 && antinode1.1 < grid[0].len() as isize){
+                    antinodes[antinode1.0 as usize][antinode1.1 as usize] = '#';
+                    antinode1.0 += delta_i;
+                    antinode1.1 += delta_j;
+                }
+            }
+        }
+    }
+
+    let mut count = 0;
+
+    for i in 0..antinodes.len(){
+        for j in 0..antinodes[i].len(){
+            if antinodes[i][j] == '#'{
+                count+=1;
+            }
+        }
+    }
+    println!("{}",count);
+
+}
